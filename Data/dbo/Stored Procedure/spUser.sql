51b2aaf20d14d64424b2300041010a8b3294d8f2
amir23reza <amir.reza.haghverdi.khu@gmail.com>
2023-06-23 17:56:27 -0400

    creating first endpoint which connects to our own db, connection to db, creating a seperate library, and creating a stored procedure

---
+++ Data/dbo/Stored Procedure/spUser.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spUser]
+	@Id nvarchar(128)
+AS
+begin
+	/* the message that indicates the number of rows that are affected by the T-SQL statement is not returned as part of the results. */
+	set nocount on;
+
+	select *
+	from [dbo].Users
+	where Id = @Id;
+
+end
---
+++ dataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,25 @@
+﻿using dataManager.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataManager.Library.DataAccess
+{
+    public class UserData
+    {   
+        public List<UserModel> GetUserById(string Id)
+        {
+            sqlDataAccess sql_ = new sqlDataAccess();
+            var p = new { Id = Id };
+            return sql_.LoadData<UserModel, dynamic>("[dbo].[spUser]", p, "DataConnection");
+            /**
+             * a couple of notes here which will be improved along the way
+             * 1- the connection name is being hard coded for now
+             * 2- we have a dependancy of the class sqlDataAccess
+             * 3- we have a dynamic type and a useless p variable
+             */
+        }
+    }
+}
